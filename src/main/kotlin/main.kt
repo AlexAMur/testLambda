f8e1a082b4a3fed98562bc10a3524a9fb04828d0
AlexAMur <muratov777sa@gmail.com>
2023-01-07 01:18:58 +0300

    task 4 final

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,10 @@
 fun main(){
     val nameList = listOf("Sasha","Juli", "Fima", "Alisa")
-
-
-    // collect only even length Strings
+    val chel = mapOf("Sasha" to 46, "Alisa" to 18, "Juli" to 45,"Fima" to 13)
     val evenFruits = nameList.fold("") { acc, e ->  acc + e }
-
     println(evenFruits)
     val fourNameList =  nameList.filter{ (it.length > 4)}.fold(""){acc, s -> acc + s }
     println(fourNameList)
+    val small = chel.filter { it.value <= 18 }
+    println(small.keys)
 }
